3dde749bde7ed8464bb5122a1c80ac59d21e7ebe
GustavoEngineer <gustavitotlgac@gmail.com>
2025-02-17 18:35:36 -0600

    New Files

---
+++ RecursiveExercises/Exercise3/Torres/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    
+    static int Torres(int NumDiscos)
+    {
+        if (NumDiscos == 1)
+        {
+            return 1;
+        }
+        return (int)Math.Pow(2, NumDiscos) - 1;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el numero de discos: ");
+        int NumDiscos = int.Parse(Console.ReadLine() ?? "0");
+        Console.WriteLine("El numero de movimientos necesarios es: " + Torres(NumDiscos));
+    }
+}
